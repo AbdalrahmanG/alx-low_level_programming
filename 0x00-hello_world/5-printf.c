d7c9db088d569c169766637cfc2cfe80fb80b89e
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-06-18 11:35:49 -0700

    C ALX

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+/**
+ * main - Entry point
+ *
+ * Description: A C that prints with printf fuction.
+ *
+ * Return: Always 0 (Sucess)
+ *
+*/
+
+ int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
