8f2d28f3e2db17b644f50cf0b9ec3498ea97ce43
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-06-18 12:27:24 -0700

    alx c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
-#include  <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 /**
- * main - Entry  point 
+ * main - Entry  point
  *
- * Return: Always 1 (Sucess) 
+ * Return: Always 1 (Sucess)
 */
 int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	write(1, q, 59);
 	return (1);
 }
