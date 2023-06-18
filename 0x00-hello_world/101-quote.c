d4059a05a9e684cdfff41a5840e1f0f9ad873892
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-06-18 12:22:42 -0700

    alx c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,12 @@
 /**
  * main - Entry  point 
  *
- * Return: Always 1 
+ * Return: Always 1 (Sucess) 
 */
 int main(void)
 {
-	char q[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	write(1, q, 59);
 	return (1);
 }
