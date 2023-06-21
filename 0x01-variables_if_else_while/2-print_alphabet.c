6bfdeada8bcb672967fca9d5189d219380122427
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-06-21 02:21:57 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	putcher(ch,'\n');
+	return (0);
+}
