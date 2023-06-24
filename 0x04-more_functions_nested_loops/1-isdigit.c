72ede9c58232c912d6903f82d63796e2b57d2664
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-06-23 19:14:08 -0700

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,8 +9,17 @@
  */
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
+	char i = '0';
+	int isdigit = 0;
+
+	for (; i <= '9'; i++)
+	{
+		if (i == c)
+		{
+			isdigit = 1;
+			break;
+		}
+	}
+
+	return (isdigit);
 }
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,8 +7,8 @@
  */
 void print_numbers(void)
 {
-	int num = 0
-		;
+	int num = 0;
+
 	do {
 		_putchar(num + 48);
 		num++;
