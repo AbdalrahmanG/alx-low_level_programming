27d62d9c9a4fe24483b107fab597b750e370b616
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-07-03 12:55:34 -0700

    done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * _strchr - a function that fills memory
+ *
+ * @s: a pointer to put string value
+ * @c: constant
+ *
+ * Return: pointer s
+ */
+char *strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
