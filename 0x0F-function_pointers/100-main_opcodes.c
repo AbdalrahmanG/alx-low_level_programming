a263bb6e1a18b5c994bc85affbf093ef5c8867da
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-07-20 08:57:39 -0700

    done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include"function_pointer.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function for checking code
+ *
+ * @argc: args number
+ * @argv: argument
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	char *j = (char *)main;
+	int i;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+
+	if (b < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
