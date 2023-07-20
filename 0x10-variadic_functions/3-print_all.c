64c96635539afcbf266e68ed41a8dd1a6280d5b1
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-07-20 11:14:21 -0700

    done

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,91 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _printchar - for printing char type
+ * @list: va_list
+ */
+
+void _printchar(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+
+
+/**
+ * _printstr - for printing string from va_list
+ * @list: va_list 
+ */
+
+void _printstr(va_list list)
+{
+	char *s;
+
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+
+
+/**
+ * _printfloat - for printing float from va_list
+ * @list: va_list
+ */
+
+void _printfloat(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+
+
+/**
+ * _printint - for printing int from va_list
+ * @list: va_list
+ */
+
+void _printint(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+
+
+/**
+ * print_all - for printing anything if it is int, char, float,  string.
+ * @format: format of strings
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	va_list args;
+	char *sep;
+
+	checker storage[] = {
+		{ "c", _printchar },
+		{ "f", _printfloat },
+		{ "s", _printstr },
+		{ "i", _printint }
+	};
+
+	i = 0;
+	sep = "";
+	va_start(args, format);
+	while (format != NULL && format[i / 4] != '\0')
+	{
+		j = i % 4;
+		if (storage[j].type[0] == format[i / 4])
+		{
+			printf("%s", sep);
+			storage[j].f(args);
+			sep = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
