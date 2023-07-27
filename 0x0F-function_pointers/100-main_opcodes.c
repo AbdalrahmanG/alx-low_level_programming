a7a14e4c8b002db38e79e37ffc164fae6b4bc4d4
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-07-27 13:25:22 -0700

    done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,19 +10,35 @@
  *
  * Return: 0
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	char *p = (char *)main;
-	int b;
+	int bytes, a;
+	char *arr;
 
 	if (argc != 2)
-		printf("Error\n"), exit(1);
-	b = atoi(argv[1]);
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
 
-	if (b < 0)
-		printf("Error\n"), exit(2);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
 
-	while (b--)
-		printf("%02hhx%s", *p++, b ? " " : "\n");
+	for (a = 0; a < bytes; a++)
+	{
+		if (a == bytes - 1)
+		{
+			printf("%02hhx\n", arr[a]);
+			break;
+		}
+		printf("%02hhx ", arr[a]);
+	}
 	return (0);
 }
+
