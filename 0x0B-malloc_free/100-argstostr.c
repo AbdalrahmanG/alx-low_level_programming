341a390733ccea7103c600a7a44cf1f05fc5150b
AbdalrahmanG <eng.abdalrahman.gomaa@gmail.com>
2023-09-05 13:33:32 -0700

    it is done this time

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - for finding the lenth of string
+ * @s: the sring it self
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+
+
+/**
  * argstostr - for concatenating all the arguments
  *
  * @ac: arguments number
